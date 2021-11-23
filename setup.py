0617b2bc26826de4baa36682226efce0932331ca
Yifan Zhang <freqyifan@gmail.com>
2021-11-23 15:09:28 +0300

    Bump version: 0.11.27 → 0.11.28

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with codecs.open(
 
 setup(
     name="tanbih-pipeline",
-    version="0.11.27",
+    version="0.11.28",
     description="a pipeline framework for streaming processing",
     long_description=readme,
     long_description_content_type="text/x-rst",
