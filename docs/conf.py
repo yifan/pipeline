7dbaedb899e89631aabf46825a8f4afa245f6955
Yifan Zhang <freqyifan@gmail.com>
2021-11-23 15:01:38 +0300

    Bump version: 0.11.26 → 0.11.27

--- docs/conf.py
+++ docs/conf.py
@@ -26,13 +26,13 @@ project = "Tanbih Pipeline"
 year = "2020"
 author = "Yifan Zhang"
 copyright = "{0}, {1}".format(year, author)
-release = "0.11.26"
+release = "0.11.27"
 # try:
 #    from pkg_resources import get_distribution
 #    version = release = get_distribution('tanbih-worker').version
 # except Exception:
 #    traceback.print_exc()
-#    version = release = '0.11.26'
+#    version = release = '0.11.27'
 
 pygments_style = "trac"
 templates_path = ["."]
